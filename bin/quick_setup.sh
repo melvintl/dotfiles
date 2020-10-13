767797161b4cf159eb91ba30ff719c7e6328d83d
melvintl <melvinl@gmail.com>
2020-10-13 16:49:22 +0100

    quick setup

--- bin/quick_setup.sh
+++ bin/quick_setup.sh
@@ -4,3 +4,5 @@ git clone https://github.com/melvintl/dotfiles.git
 ln -s -f ~/myprojects/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s -f ~/myprojects/dotfiles/.tmux.conf.local ~/.tmux.conf.local
 ln -s -f ~/myprojects/dotfiles/.vimrc ~/.vimrc
+ln -s -f ~/myprojects/dotfiles/bin/tmux-session  ~/.tmux-session 
+
