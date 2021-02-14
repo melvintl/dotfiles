714250a1c831cb117ada95330b6043fac3504359
melvintl <melvin@DESKTOP-9B007UL.localdomain>
2021-02-14 18:49:49 +0000

    updates from wsl

--- bin/quick_setup.sh
+++ bin/quick_setup.sh
@@ -1,6 +1,7 @@
 # mkdir -p ~/myprojects
 # cd myprojects
 # git clone https://github.com/melvintl/dotfiles.git
+
 ln -s -f $(pwd)/.tmux.conf ~/.tmux.conf
 ln -s -f $(pwd)/.tmux.conf.local ~/.tmux.conf.local
 ln -s -f $(pwd)/.vimrc ~/.vimrc
