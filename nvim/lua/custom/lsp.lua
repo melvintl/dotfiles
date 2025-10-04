13f2fcc21d6b0f2ea98f04424be6f4aaf84fc26e
Melvin <melvin@xyz.com>
2025-10-04 20:56:23 +0100

    minor fixes/updates

--- nvim/lua/custom/lsp.lua
+++ nvim/lua/custom/lsp.lua
@@ -2,7 +2,7 @@
 local lspconfig = require('lspconfig')
 -- lspconfig.pyright.setup {}
 lspconfig.jedi_language_server.setup {}
-lspconfig.tsserver.setup {}  -- This is the correct name for TypeScript server
+lspconfig.ts_ls.setup {} 
 lspconfig.eslint.setup {}
 lspconfig.rust_analyzer.setup {
   -- Server-specific settings. See `:help lspconfig-setup`
