d4d4344bc57a1822594bd041e048228eaf326aa6
Melvin <melvin@xyz.com>
2026-09-20 05:25:50 +0100

    Convert Vimscript globals to Lua

--- nvim/lua/custom/startify.lua
+++ nvim/lua/custom/startify.lua
@@ -1,11 +1,9 @@
-vim.cmd [[
-let g:startify_change_to_dir = 0
-let g:startify_change_to_vcs_root = 1
-" Use :SS to save a session
-let g:startify_session_persistence = 1
-let g:startify_files_number = 5
-let g:startify_lists = [
-            \ { 'type': 'sessions', 'header': ['   Sessions'] },
-            \ { 'type': 'dir',      'header': ['   Recent Files'] },
-            \ ]
-]]
+vim.g.startify_change_to_dir = 0
+vim.g.startify_change_to_vcs_root = 1
+-- Use :SS to save a session
+vim.g.startify_session_persistence = 1
+vim.g.startify_files_number = 5
+vim.g.startify_lists = {
+  { type = 'sessions', header = { '   Sessions' } },
+  { type = 'dir', header = { '   Recent Files' } },
+}
